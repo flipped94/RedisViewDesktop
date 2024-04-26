6630f12761561d6f504ca439ffc9c5548332c36c
flipped <1123630646@qq.com>
2024-04-26 08:31:47 +0800

    extract KeyFill variable

--- RedisViewDesktop/Models/NewKey.cs
+++ RedisViewDesktop/Models/NewKey.cs
@@ -13,18 +13,20 @@ namespace RedisViewDesktop.Models
             KeyType = keyType;
         }
 
-        public static IEnumerable<NewKey> GetNewKeys()
-        {
-            return new List<NewKey>()
-            {
+        private static readonly List<NewKey> KeyFill = 
+            [
                 new("#cdddf8","HASH"),
                 new("#a5d4c3","LIST"),
                 new("#d4baa7","SET"),
                 new("#d9a0c6","ZSET"),
                 new("#c7b0ea","STRING"),
                 new("#b8c5db","JSON"),
-                new("#c7cea8","STREAM")
-            };
+                new("#c7cea8","STREAM"),
+            ];
+
+        public static IEnumerable<NewKey> GetNewKeys()
+        {
+            return KeyFill;
         }
     }
 }
